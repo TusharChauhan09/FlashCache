29b6e96faf8d813f6f89a35867d834dd007f7b67
Tushar Chauhan <tusharchauhan0912@gmail.com>
2026-05-07 20:07:26 +0530

    key-value-map & queue imp

--- internal/store/kv.go
+++ internal/store/kv.go
@@ -1,2 +1,43 @@
 // ! SET / GET / DEL / EXISTS
 package store
+
+func (s *Store) Set(key, value string ){
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.kv[key] = value
+}
+
+func (s *Store) Get(key string) (string,error){
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	value, exist := s.kv[key]  // ok==exist
+	if !exist {
+		return "",ErrKeyNotFound
+	} 
+
+	return value, nil
+}
+
+func (s *Store) Delete(key string) bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	_ , exist := s.kv[key]
+	if !exist{
+		return false
+	}
+	
+	delete(s.kv,key)
+	return true
+}
+
+func (s *Store) Exist(key string) bool {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	_ , exist := s.kv[key]
+
+	return exist
+}
--- internal/store/queue.go
+++ internal/store/queue.go
@@ -1,2 +1,31 @@
 // !  PUSH / POP / LEN
 package store
+
+func (s *Store) Push(queueName , value string){
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.queues[queueName] = append(s.queues[queueName], value)
+}
+
+func (s *Store) Pop(queueName string) (string, error){
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	queue, exists := s.queues[queueName]
+	if !exists || len(queue) == 0 {
+		return "", ErrQueueEmpty
+	}
+
+	value := queue[0]
+	s.queues[queueName] = queue[1:]
+
+	return value, nil
+}
+
+func (s *Store) Len (queueName string) int {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	return len(s.queues[queueName])
+}
